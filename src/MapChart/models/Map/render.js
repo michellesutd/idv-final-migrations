3635622e73ab440bc0c3cf2c2799cce5aff90da3
michellesutd <michelleboosj@gmail.com>
2020-05-01 00:03:00 +0800

    fixed labels

--- src/MapChart/models/Map/render.js
+++ src/MapChart/models/Map/render.js
@@ -22,7 +22,6 @@ Render.drawLinks = function (trans_point, links, ctx, projection, style) {
 
   for (let i = 0; i < links.length; i++) {
     const d = links[i];
-
     drawText(d.source)
     drawText(d.target)
   }
@@ -30,9 +29,13 @@ Render.drawLinks = function (trans_point, links, ctx, projection, style) {
   function drawText(d) {
     if (drawn_places.indexOf(d.place) !== -1) return
     drawn_places.push(d.place)
-    ctx.fillStyle = "black";
-    ctx.font = 10+'px sans-serif';
     ctx.textAlign = d.coor[0] > 0 ? "start" : "end";
+    ctx.font = 10+'px sans-serif';
+
+    ctx.strokeStyle = "white";
+    ctx.strokeText(d.place, ...projection(d.coor));
+
+    ctx.fillStyle = "black";
     ctx.fillText(d.place, ...projection(d.coor));
   }
 }
